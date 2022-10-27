2cbe8b558fa05a29497a69053dd95ce0a3e35332
zdw <1099509472@qq.com>
2022-10-27 09:06:23 +0800

    ?

---
+++ day03/day03/src/Pratice.java
@@ -0,0 +1,52 @@
+/*
+练习：找出三个数中的最大值
+ */
+
+class Pratice {
+    public static void main(String[] args) {
+       /* int num1 = 12, num2 = 26, num3 = 75;
+        float max;
+        if (num1 >= num2 && num1 >= num3) {
+            max = num1;
+        }else if (num2 >= num1 && num2 >= num3) {
+            max = num2;
+            //System.out.println("num2是最大值");
+
+        }else{
+            max = num3;
+
+            //System.out.println("num3是最大值");
+        }
+    System.out.println("三个数最大值为：" + max);
+    }
+*/
+       /*
+        float d1 = 12.3f, d2 = 53.2f, d3, d4;
+        if (d1 < 10 && d2 < 20) {
+            d3 = d1 + d2;
+            System.out.println("两数的和为：" + d3);
+        } else {
+            d4 = d1 - d2;
+            System.out.println("两数的差为：" + d4);
+        }
+*/
+        /*
+        交换两个变量代码实现
+         */
+        String s1 = "陕西科技大学";
+        String s2 = "西北工业大学";
+        String temp3;
+       temp3 = s2;
+        s2 = s1;
+        s1 = temp3;
+
+
+        System.out.println("s1为：" + s1);
+        System.out.println("s2为：" + s2);
+        System.out.println("temp3为：" + temp3);
+
+    }
+}
+
+
+
